496e3efa506b3236583d1fa72a4128a99e21d542
stonezz <pascin123>
2018-07-05 15:45:11 +0200

    no raise

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -4,11 +4,13 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "v1.18";
+    static final String VERSION = "v1.20";
 
     public static int betRequest(JsonElement request) {
 
@@ -24,24 +26,65 @@ public class Player {
 
         int currentBuyIn = jobject.get("current_buy_in").getAsInt();
 
-        int betAll = 0;
-
         JsonObject player = players.get(5).getAsJsonObject();
         int bet=  player.get("bet").getAsInt();
 
-       // for(int i=0; i<players.size(); i++) {
-       //    JsonObject player = players.get(i).getAsJsonObject();
-       //    int bet=  player.get("bet").getAsInt();
-        //   if(bet >= betAll) {
-        //       betAll = bet;
-        //   }
-        //}
+        JsonArray cards = player.get("hole_cards").getAsJsonArray();
 
-        return currentBuyIn - bet + 50;
+
+        List<Card> karten = new ArrayList<>();
+
+      for(int i=0;i< cards.size();i++) {
+          JsonObject card = cards.get(i).getAsJsonObject();
+
+          String wertStr = card.get("rank").getAsString();
+          String farbe = card.get("suit").getAsString();
+
+          int wert = getWert(wertStr);
+          
+          Card c = new Card(wert, farbe);
+          karten.add(c);
+      }
+
+        return currentBuyIn - bet;
+    }
+
+    private static int getWert(String wertStr) {
+
+        int wert = 0;
+
+        try {
+
+           if (wertStr.equals("K")) {
+               wert = 13;
+           } else if (wertStr.equals("A")) {
+               wert = 14;
+           } else if (wertStr.equals("Q")) {
+               wert = 12;
+           } else if (wertStr.equals("J")) {
+               wert = 11;
+           } else {
+               wert = Integer.valueOf(wertStr);
+           }
+       }
+       catch(Exception e) {
+System.out.println(e);
+       }
+        return wert;
     }
 
     public static void showdown(JsonElement game) {
 
 
+    }
+
+    static class Card {
+        public int wert;
+        public String farbe;
+
+        public Card(int wert, String farbe) {
+            this.wert = wert;
+            this.farbe = farbe;
+        }
     }
 }
