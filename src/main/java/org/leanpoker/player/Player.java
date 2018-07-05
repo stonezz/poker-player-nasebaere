36191e2f183c3240fbcf24838f270e150e3d9687
stonezz <pascin123>
2018-07-05 16:10:32 +0200

    bett bet

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -4,13 +4,11 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Player {
 
-    static final String VERSION = "v1.20";
+    static final String VERSION = "v1.21";
 
     public static int betRequest(JsonElement request) {
 
@@ -30,23 +28,74 @@ public class Player {
         int bet=  player.get("bet").getAsInt();
 
         JsonArray cards = player.get("hole_cards").getAsJsonArray();
+        List<Card> kartenInHand =  getKarten(cards);
 
 
+        cards = jobject.get("community_cards").getAsJsonArray();
+        List<Card> kartenAufTisch =  getKarten(cards);
+
+        Map<Integer, Integer> kartenMap = new TreeMap<>(Collections.<Integer>reverseOrder());
+
+        List<Card> alleKarten = new ArrayList<>();
+        alleKarten.addAll(alleKarten);
+        alleKarten.addAll(kartenAufTisch);
+
+        for(Card karte : alleKarten) {
+            Integer anzahlKarten = kartenMap.get(new Integer(karte.wert));
+            if(anzahlKarten == null) {
+                kartenMap.put(new Integer(karte.wert), new Integer(1));
+            }
+            else {
+                kartenMap.put(new Integer(karte.wert), ++anzahlKarten);
+            }
+        }
+
+        if(alleKarten.size() > 0) {
+            Card highestCard = alleKarten.get(0);
+        }
+
+
+     boolean  raise = false;
+        if(kartenAufTisch.size() == 0) {
+          raise  = hasMindPairs(kartenMap);
+        }
+
+        int bett = currentBuyIn - bet;
+if(raise) {
+    bett += 10;
+}
+
+
+
+        return bett;
+    }
+
+    private static boolean hasMindPairs(Map<Integer, Integer> kartenMap) {
+        for(Integer wert :  kartenMap.values()) {
+            if(wert.intValue() > 1)
+                return true;
+        }
+
+        return false;
+    }
+
+
+    private static List<Card> getKarten(JsonArray cards) {
         List<Card> karten = new ArrayList<>();
 
-      for(int i=0;i< cards.size();i++) {
-          JsonObject card = cards.get(i).getAsJsonObject();
+        for(int i=0;i< cards.size();i++) {
+            JsonObject card = cards.get(i).getAsJsonObject();
 
-          String wertStr = card.get("rank").getAsString();
-          String farbe = card.get("suit").getAsString();
+            String wertStr = card.get("rank").getAsString();
+            String farbe = card.get("suit").getAsString();
 
-          int wert = getWert(wertStr);
-          
-          Card c = new Card(wert, farbe);
-          karten.add(c);
-      }
+            int wert = getWert(wertStr);
 
-        return currentBuyIn - bet;
+            Card c = new Card(wert, farbe);
+            karten.add(c);
+        }
+
+        return karten;
     }
 
     private static int getWert(String wertStr) {
