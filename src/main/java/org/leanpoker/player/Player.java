fc43ea1372ccbd0b7fa0b2d285b87cc18384b631
stonezz <pascin123>
2018-07-05 13:47:42 +0200

    up

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,17 +1,25 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "stonezz";
+    static final String VERSION = "stonezz v1.12";
 
     public static int betRequest(JsonElement request) {
-    	
-    	
-        return 200;
+
+       JsonObject jobject = request.getAsJsonObject();
+      JsonArray jarray = jobject.getAsJsonArray("players");
+
+   //     for(int i=0; i<jarray.size(); i++) {
+ //  JsonObject item = items2.get(i).getAsJsonObject();
+ //System.out.println(item.get("start").getAsString());}
+
+        return 1200;
     }
 
     public static void showdown(JsonElement game) {
